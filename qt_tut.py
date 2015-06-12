2ab12b03e65d9b994bef269afc4b1a19e076e97f
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-06-12 18:49:47 -0500

    Quit button

--- qt_tut.py
+++ qt_tut.py
@@ -13,35 +13,28 @@ website: zetcode.com
 last edited: October 2011
 """
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 
 class Example(QtGui.QWidget):
-    
     
     def __init__(self):
         super(Example, self).__init__()
         
         self.initUI()
-
-    def initUI(self):
         
-        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+    def initUI(self):               
         
-        self.setToolTip('This is a <b>QWidget</b> widget')
+        qbtn = QtGui.QPushButton('Quit', self)
+        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 50)       
         
-        btn = QtGui.QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushButton</b> widget')
-        btn.resize(btn.sizeHint())
-        btn.move(50,50)
-        
-        self.setGeometry(300,300,250,150)
-        self.setWindowTitle('Tooltips')
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Quit button')    
         self.show()
-
-
+        
 def main():
-    
     
     app = QtGui.QApplication(sys.argv)
     ex = Example()
