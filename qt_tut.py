f018a37f3cb923d12d7b5ed24f081c1d2ba27fbf
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-06-12 19:17:00 -0500

    Simple message box

--- qt_tut.py
+++ qt_tut.py
@@ -13,29 +13,32 @@ website: zetcode.com
 last edited: October 2011
 """
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
 
 
 class Example(QtGui.QWidget):
-    
+
     def __init__(self):
         super(Example, self).__init__()
-        
         self.initUI()
-        
-    def initUI(self):               
-        
-        qbtn = QtGui.QPushButton('Quit', self)
-        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
-        qbtn.resize(qbtn.sizeHint())
-        qbtn.move(50, 50)       
-        
+
+    def initUI(self):
         self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Quit button')    
+        self.setWindowTitle('Message Box')
         self.show()
+
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, 'Message', 
+            "Are you certain of this?", QtGui.QMessageBox.Yes |
+            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
         
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
 def main():
-    
     app = QtGui.QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
