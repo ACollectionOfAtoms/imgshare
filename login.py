157817f96fd40d96be300c5dfb771abc6d617058
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-07-13 11:56:45 -0500

    added imgurpython and webbrowser modules using the imgurpython module, I have hard-coded the API keys required for use. These are then used to generate an authorization URL, which is opened by the webbrowser module. A button was added to launch users default browser to obtain pin.

--- login.py
+++ login.py
@@ -1,33 +1,38 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 12 13:30:14 2015
 
-@author: Adam
-
-"""
+import sys
 from PyQt4 import QtGui
+from webbrowser import open_new_tab
+from imgurpython import ImgurClient
 
 
 class Login(QtGui.QDialog):
-
     def __init__(self):
-        super(Login, QtGui.QDialog).__init__(self)
-        self.textName = QtGui.QLineEdit(self)
-        self.textPass = QtGui.QLineEdit(self)
+        QtGui.QDialog.__init__(self)
+        self.textpin = QtGui.QLineEdit(self)
         self.buttonLogin = QtGui.QPushButton('Login', self)
-        self.buttonLogin.clicked.connect(self.handleLogin)
-        layout = QtGui.QVBoxLayout(self)
-        layout.addWidget(self.textName)
-        layout.addWidget(self.textPass)
-        layout.addWidget(self.buttonLogin)
+        self.buttonPin = QtGui.QPushButton('Get PIN', self)
 
-    def handleLogin(self):
-        if (self.textName.text() == 'foo' and
-            self.textPass.text() == 'bar'):
-            self.accept()
-        else:
-            QtGui.QMessageBox.warning(
-                self, 'Error', 'Bad user or password')
+        self.buttonLogin.clicked.connect(self.handle_pin)
+        self.buttonPin.clicked.connect(self.authenticate)
+        layout = QtGui.QVBoxLayout(self)
+        layout.addWidget(self.textpin)
+        layout.addWidget(self.buttonLogin)
+        layout.addWidget(self.buttonPin)
+
+    def authenticate(self):
+        #authenticate imgur user login
+        client_id = '912116b2944a624'
+        client_secret = 'c4fc62a0b62338f9e25f9062147e2d0ca44f428e'
+        client = ImgurClient(client_id, client_secret)
+        authorization_url = client.get_auth_url('pin')
+        open_new_tab(authorization_url)
+
+    def handle_pin(self):
+        pass
+
+
 
 class Window(QtGui.QMainWindow):
     def __init__(self):
@@ -42,3 +47,7 @@ if __name__ == '__main__':
         window = Window()
         window.show()
         sys.exit(app.exec_())
+
+
+
+
