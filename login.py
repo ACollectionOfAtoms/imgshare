115f75cb1924e109bd8316717fbad733f1339f38
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-07-14 11:11:22 -0500

    Login window fully functional... imgurpython module allows for succesful authentication still need to work on main program

--- login.py
+++ login.py
@@ -5,33 +5,40 @@ import sys
 from PyQt4 import QtGui
 from webbrowser import open_new_tab
 from imgurpython import ImgurClient
+from imgurpython.helpers.error import ImgurClientError
 
 
 class Login(QtGui.QDialog):
     def __init__(self):
         QtGui.QDialog.__init__(self)
+        client_id = '912116b2944a624'
+        client_secret = 'c4fc62a0b62338f9e25f9062147e2d0ca44f428e'
+        self.client = ImgurClient(client_id, client_secret)
         self.textpin = QtGui.QLineEdit(self)
         self.buttonLogin = QtGui.QPushButton('Login', self)
         self.buttonPin = QtGui.QPushButton('Get PIN', self)
 
         self.buttonLogin.clicked.connect(self.handle_pin)
         self.buttonPin.clicked.connect(self.authenticate)
+
         layout = QtGui.QVBoxLayout(self)
         layout.addWidget(self.textpin)
-        layout.addWidget(self.buttonLogin)
         layout.addWidget(self.buttonPin)
+        layout.addWidget(self.buttonLogin)
 
     def authenticate(self):
         #authenticate imgur user login
-        client_id = '912116b2944a624'
-        client_secret = 'c4fc62a0b62338f9e25f9062147e2d0ca44f428e'
-        client = ImgurClient(client_id, client_secret)
-        authorization_url = client.get_auth_url('pin')
+        authorization_url = self.client.get_auth_url('pin')
         open_new_tab(authorization_url)
 
     def handle_pin(self):
-        pass
+        try:
+            self.client.authorize(str(self.textpin.text()), 'pin')
+            self.accept()
+            return self.client
 
+        except ImgurClientError as e:
+            QtGui.QMessageBox.warning(self, str(e.status_code), str(e.error_message))
 
 
 class Window(QtGui.QMainWindow):
