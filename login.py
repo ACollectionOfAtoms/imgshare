2d7c88e3f837f27450aada518a7a59cdae932ccf
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-07-15 10:32:51 -0500

    Added some color to the login window

--- login.py
+++ login.py
@@ -3,7 +3,7 @@
 
 import sys
 import tray
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from webbrowser import open_new_tab
 from imgurpython import ImgurClient
 from imgurpython.helpers.error import ImgurClientError
@@ -14,6 +14,8 @@ class Login(QtGui.QDialog):
         QtGui.QDialog.__init__(self)
         client_id = '912116b2944a624'
         client_secret = 'c4fc62a0b62338f9e25f9062147e2d0ca44f428e'
+        self.message = QtGui.QLabel('Enter PIN')
+        self.message.setAlignment(QtCore.Qt.AlignCenter)
         self.client = ImgurClient(client_id, client_secret)
         self.textpin = QtGui.QLineEdit(self)
         self.buttonLogin = QtGui.QPushButton('Login', self)
@@ -22,7 +24,13 @@ class Login(QtGui.QDialog):
         self.buttonLogin.clicked.connect(self.handle_pin)
         self.buttonPin.clicked.connect(self.authenticate)
 
+        self.setStyleSheet("""
+            background-color: rgb(49,49,49);
+            color: rgb(255,255,255);
+    """)
+
         layout = QtGui.QVBoxLayout(self)
+        layout.addWidget(self.message)
         layout.addWidget(self.textpin)
         layout.addWidget(self.buttonPin)
         layout.addWidget(self.buttonLogin)
