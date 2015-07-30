cf6bfaaf93626639c1b931d57fd0236297047d88
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-07-29 21:09:55 -0500

    adding some window boilerplate

--- options.py
+++ options.py
@@ -1,1 +1,17 @@
-from PyQt5 import QtGui, QtCore, QtWidgets
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class OptionWindow(QWidget):
+
+    def __init__(self):
+        super(OptionWindow, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(500,500,500,320)
+        self.setWindowTitle('Options')
+
+        self.show()
