373569416f599bbf09b7bbc1ab9842472eab5473
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-08-07 17:34:30 -0500

    Shebang!

--- options.py
+++ options.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from PyQt5.QtWidgets import (QWidget, QDesktopWidget, QVBoxLayout, QHBoxLayout, QCheckBox, QGridLayout,
                              QLabel, QPushButton, QLineEdit, QComboBox)
 from PyQt5.QtGui import QIcon
